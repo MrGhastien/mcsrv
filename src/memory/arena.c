6e46a087bb88a6a7ee50ed1374db9f4ef5fd2b82
MrGhastien <bastounm@free.fr>
2024-06-05 01:39:06 +0200

    feat(memory): Add a linear memory allocator

---
+++ src/memory/arena.c
@@ -0,0 +1,36 @@
+#include "arena.h"
+#include "../utils/bitwise.h"
+
+#include <stdlib.h>
+#define DEFAULT_CAP 64
+
+Arena arena_create() {
+    Arena res = {
+        .block = malloc(DEFAULT_CAP),
+        .capacity = DEFAULT_CAP,
+        .length = 0
+    };
+    if(!res.block)
+        res.capacity = 0;
+    return res;
+}
+
+void arena_destroy(Arena* arena) {
+    free(arena->block);
+}
+
+void* arena_allocate(Arena* arena, size_t bytes) {
+    size_t final_length = arena->length + bytes;
+    if(final_length > arena->capacity) {
+        size_t new_cap = ceil_two_pow(final_length);
+        void* new_block = realloc(arena->block, new_cap);
+        if(!new_block)
+            return NULL;
+
+        arena->block = new_block;
+        arena->capacity = new_cap;
+    }
+    void* ptr = arena->block + arena->length;
+    arena->length = final_length;
+    return ptr;
+}
---
+++ src/memory/arena.h
@@ -0,0 +1,18 @@
+#ifndef LINALLOC_H
+#define LINALLOC_H
+
+#include "../definitions.h"
+#include <stddef.h>
+
+typedef struct linalloc {
+    void* block;
+    size_t capacity;
+    size_t length;
+} Arena;
+
+Arena arena_create();
+void arena_destroy(Arena* arena);
+
+void* arena_allocate(Arena* arena, size_t bytes);
+
+#endif /* ! LINALLOC_H */
