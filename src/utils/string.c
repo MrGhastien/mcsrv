dcb1ec022c9f6c38094457b6de9c2ab0a66f78ec
MrGhastien <bastounm@free.fr>
2024-06-06 02:36:01 +0200

    feat(string): Add more functions to create strings, actually implement the string functions

---
+++ src/utils/string.c
@@ -0,0 +1,52 @@
+#include "string.h"
+#include <stdlib.h>
+#include <string.h>
+
+string str_init(const char* cstr, size_t length, Arena* arena) {
+    bool allocated = FALSE;
+    if (!arena) {
+        arena = malloc(sizeof *arena);
+        *arena = arena_create();
+        allocated = TRUE;
+    }
+
+    string str = {
+        .arena = arena,
+        .length = length,
+        .allocated = allocated,
+    };
+
+    str.base = arena_allocate(arena, str.length + 1);
+    memcpy(str.base, cstr, str.length);
+    str.base[str.length] = 0;
+    return str;
+}
+
+string str_create(const char* cstr) {
+    return str_init(cstr, strlen(cstr), NULL);
+}
+
+string str_create_with_buffer(const char* cstr, size_t length) {
+    return str_init(cstr, length, NULL);
+}
+
+string str_create_with_arena(const char* cstr, Arena* arena) {
+    return str_init(cstr, strlen(cstr), arena);
+}
+
+void str_destroy(string* str) {
+    arena_free(str->arena, (str->length + 1) * sizeof(char));
+    if (str->allocated)
+        free(str->arena);
+}
+
+void str_set(string* str, const char* cstr) {
+    size_t len = strlen(cstr);
+
+    if (len < str->length)
+        arena_free(str->arena, str->length - len);
+    else
+        arena_allocate(str->arena, len - str->length);
+
+    memcpy(str->base, cstr, len + 1);
+}
--- src/utils/string.h
+++ src/utils/string.h
@@ -2,19 +2,23 @@
 #define STRING_H
 
 #include "../definitions.h"
+#include "../memory/arena.h"
 #include <stddef.h>
 
 typedef struct str {
-    char* chars;
+    Arena* arena;
+    char* base;
     size_t length;
     bool allocated;
 } string;
 
-string str_create(char* cstr);
-string str_create_with_buffer(char* cstr, size_t length);
+string str_init(const char* cstr, size_t length, Arena* arena);
+string str_create(const char* cstr);
+string str_create_with_buffer(const char* cstr, size_t length);
+string str_create_with_arena(const char* cstr, Arena* arena);
 
 void str_destroy(string* str);
 
-
+void str_set(string* str, const char* cstr);
 
 #endif /* ! STRING_H */
