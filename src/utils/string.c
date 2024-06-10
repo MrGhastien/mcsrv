50b512c7710c72fb6bdd1b6adee56bba9ac8b1cd
Bastien Morino <bastien.morino@free.fr>
2024-06-10 21:15:42 +0200

    refactor(string): Remove dependency on Arenas, fix invalid size computations & memory copies

--- src/utils/string.c
+++ src/utils/string.c
@@ -1,63 +1,128 @@
 #include "string.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-string str_init(const char* cstr, size_t length, Arena* arena) {
-    bool allocated = FALSE;
-    if (!arena) {
-        arena = malloc(sizeof *arena);
-        *arena = arena_create();
-        allocated = TRUE;
+string str_init(const char* cstr, size_t length, size_t capacity, Arena* arena) {
+    string str;
+    if(capacity == 0)
+        str.base = (char*)cstr;
+    else if (!arena)
+        str.base = malloc(capacity);
+    else
+        str.base = arena_allocate(arena, capacity);
+
+    if (!str.base) {
+        str.fixed = TRUE;
+        str.capacity = 0;
+        str.length = 0;
+        return str;
     }
 
-    string str = {
-        .arena = arena,
-        .length = length,
-        .allocated = allocated,
-    };
+    str.fixed = capacity == 0 || arena != NULL;
+    str.length = length;
+    str.capacity = capacity;
 
-    str.base = arena_allocate(arena, str.length + 1);
-    memcpy(str.base, cstr, str.length);
-    str.base[str.length] = 0;
+    if(!str_is_const(&str)) {
+        str.base[str.length] = 0;
+        if(cstr)
+            memcpy(str.base, cstr, length);
+    }
     return str;
 }
 
 string str_create(const char* cstr) {
-    return str_init(cstr, strlen(cstr), NULL);
+    size_t len = strlen(cstr);
+    return str_init(cstr, len, len + 1, NULL);
 }
 
-string str_create_with_buffer(const char* cstr, size_t length) {
-    return str_init(cstr, length, NULL);
+string str_create_const(const char* cstr) {
+    return str_init(cstr, strlen(cstr), 0, NULL);
 }
 
-string str_alloc(const char* cstr, Arena* arena) {
-    return str_init(cstr, strlen(cstr), arena);
+string str_alloc(size_t capacity, Arena* arena) {
+    return str_init(NULL, 0, capacity, arena);
+}
+
+bool str_is_const(const string* str) {
+    return str->capacity == 0;
 }
 
 void str_destroy(string* str) {
-    arena_free(str->arena, (str->length + 1) * sizeof(char));
-    if (str->allocated)
-        free(str->arena);
+    if (str_is_const(str))
+        return;
+    if (!str->fixed)
+        free(str->base);
 }
 
 void str_set(string* str, const char* cstr) {
+    if (str_is_const(str))
+        return;
     size_t len = strlen(cstr);
 
-    if (len < str->length)
-        arena_free(str->arena, str->length - len);
-    else
-        arena_allocate(str->arena, len - str->length);
+    if (!str->fixed)
+        str->base = realloc(str->base, len + 1);
+    else if (len > str->capacity)
+        return;
 
     memcpy(str->base, cstr, len + 1);
+    str->length = len;
 }
 
 void str_copy(string* dst, const string* src) {
+    if (str_is_const(dst))
+        return;
     size_t len = src->length;
 
-    if (len < dst->length)
-        arena_free(dst->arena, dst->length - len);
-    else
-        arena_allocate(dst->arena, len - dst->length);
+    if (!dst->fixed)
+        dst->base = realloc(dst->base, len + 1);
+    else if (len > dst->capacity)
+        return;
 
     memcpy(dst->base, src->base, len + 1);
+    dst->length = len;
+}
+
+void str_append(string* str, const char* cstr) {
+    if (str_is_const(str)) {
+        fputs("Attempted to concat something to a const string!", stderr);
+        return;
+    }
+    size_t len = strlen(cstr);
+
+    if (!str->fixed && str->length + len >= str->capacity) {
+        size_t new_cap = str->capacity + (str->capacity >> 1);
+        char* new_base = realloc(str->base, new_cap);
+        if(new_base) {
+            str->capacity = new_cap;
+            str->base = new_base;
+        }
+    }
+
+    str->length = strlcat(str->base, cstr, str->capacity);
+    if (str->length >= str->capacity)
+        str->length = str->capacity - 1;
+    str->base[str->length] = 0;
+}
+
+void str_concat(string* lhs, const string* rhs) {
+    if (str_is_const(lhs)) {
+        fputs("Attempted to concat something to a const string!", stderr);
+        return;
+    }
+    size_t len = rhs->length;
+
+    if (!lhs->fixed && lhs->length + len >= lhs->capacity) {
+        size_t new_cap = lhs->capacity + (lhs->capacity >> 1);
+        char* new_base = realloc(lhs->base, new_cap);
+        if(new_base) {
+            lhs->capacity = new_cap;
+            lhs->base = new_base;
+        }
+    }
+
+    lhs->length = strlcat(lhs->base, rhs->base, lhs->capacity);
+    if (lhs->length >= lhs->capacity)
+        lhs->length = lhs->capacity - 1;
+    lhs->base[lhs->length] = 0;
 }
--- src/utils/string.h
+++ src/utils/string.h
@@ -6,18 +6,20 @@
 #include <stddef.h>
 
 typedef struct str {
-    Arena* arena;
     char* base;
     size_t length;
-    bool allocated;
+    size_t capacity;
+    bool fixed;
 } string;
 
-string str_init(const char* cstr, size_t length, Arena* arena);
+string str_init(const char* cstr, size_t length, size_t capacity, Arena* arena);
 string str_create(const char* cstr);
-string str_create_with_buffer(const char* cstr, size_t length);
-string str_alloc(const char* cstr, Arena* arena);
+string str_create_const(const char* cstr);
+string str_alloc(size_t capacity, Arena* arena);
 
 void str_destroy(string* str);
+
+bool str_is_const(const string* str);
 
 /**
    Set the string to the passed C string.
@@ -25,5 +27,8 @@ void str_destroy(string* str);
  */
 void str_set(string* str, const char* cstr);
 void str_copy(string* dst, const string* src);
+
+void str_append(string* str, const char* cstr);
+void str_concat(string* lhs, const string* rhs);
 
 #endif /* ! STRING_H */
