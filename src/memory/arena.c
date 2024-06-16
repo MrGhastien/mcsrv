e0cd7826b2f2e925eedc110e4b24daf0ff8ae240
MrGhastien <bastounm@free.fr>
2024-06-17 01:58:46 +0200

    refactor(arena): Check for other debug macro to avoid polluting logs with allocation messages

--- src/memory/arena.c
+++ src/memory/arena.c
@@ -5,11 +5,11 @@
 #include <string.h>
 
 Arena arena_create(size_t size) {
-    Arena res = {.block = malloc(size), .capacity = size, .length = 0};
+    Arena res = {.block = malloc(size), .capacity = size, .length = 0, .saved_length = 0};
     if (!res.block)
         res.capacity = 0;
 
-#ifdef DEBUG
+#ifdef DEBUG2
     printf("Create arena %p of %zu bytes.\n", res.block, res.capacity);
 #endif
 
@@ -18,7 +18,7 @@ Arena arena_create(size_t size) {
 
 void arena_destroy(Arena* arena) {
     free(arena->block);
-#ifdef DEBUG
+#ifdef DEBUG2
     printf("Destroyed arena %p (%zu / %zu).\n", arena->block, arena->length, arena->capacity);
 #endif
     arena->block = NULL;
@@ -31,12 +31,13 @@ void* arena_allocate(Arena* arena, size_t bytes) {
         fprintf(stderr, "Tried to allocate %zu bytes, but only %zu are available.\n", bytes,
                 arena->capacity - arena->length);
 #endif
+        abort();
         return NULL;
     }
 
     void* ptr = offset(arena->block, arena->length);
     arena->length += bytes;
-#ifdef DEBUG
+#ifdef DEBUG2
     printf("Allocated %zu bytes from %p (%zu/%zu).\n", bytes, arena->block, arena->length, arena->capacity);
 #endif
     return ptr;
@@ -51,7 +52,7 @@ void arena_free(Arena* arena, size_t bytes) {
     if (bytes > arena->length)
         bytes = arena->length;
     arena->length -= bytes;
-#ifdef DEBUG
+#ifdef DEBUG2
     printf("Free %zu bytes from %p (%zu/%zu).\n", bytes, arena->block, arena->length, arena->capacity);
 #endif
 }
