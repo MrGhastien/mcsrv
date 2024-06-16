16cebb5d1f57b49bae40acf6d0481980e0a72deb
MrGhastien <bastounm@free.fr>
2024-06-17 01:56:05 +0200

    feat(definitions): Add missing type definitions

--- src/definitions.h
+++ src/definitions.h
@@ -2,9 +2,19 @@
 #define DEFINITIONS_H
 
 typedef unsigned char u8;
-typedef unsigned char bool;
-typedef unsigned short u16;
+typedef unsigned short int u16;
+typedef unsigned int u32;
 typedef unsigned long int u64;
+
+typedef signed char i8;
+typedef signed short int i16;
+typedef signed int i32;
+typedef signed long int i64;
+
+typedef float f32;
+typedef double f64;
+
+typedef unsigned char bool;
 
 #define FALSE (bool)0
 #define TRUE (bool)1
