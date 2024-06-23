347f86754fee3ebdeed2d7be9f4831a285d102de
MrGhastien <bastounm@free.fr>
2024-06-24 00:52:23 +0200

    fix(Makefile): Update source file listings

--- headers.mk
+++ headers.mk
@@ -1,14 +1,15 @@
 HDRS := $(SRC_DIR)/definitions.h \
+		$(SRC_DIR)/logger.h \
 		$(SRC_DIR)/utils/bitwise.h \
 		$(SRC_DIR)/utils/string.h \
 		$(SRC_DIR)/memory/dyn_arena.h \
 		$(SRC_DIR)/memory/arena.h \
 		$(SRC_DIR)/containers/dict.h \
 		$(SRC_DIR)/containers/vector.h \
+		$(SRC_DIR)/containers/ring_queue.h \
 		$(SRC_DIR)/network/packet.h \
 		$(SRC_DIR)/network/decoders.h \
 		$(SRC_DIR)/network/utils.h \
-		$(SRC_DIR)/logger.h \
 		$(SRC_DIR)/network/connection.h \
 		$(SRC_DIR)/network/bytebuffer.h \
 		$(SRC_DIR)/network/receiver.h \
@@ -16,6 +17,7 @@ HDRS := $(SRC_DIR)/definitions.h \
 		$(SRC_DIR)/network/network.h \
 		$(SRC_DIR)/network/handlers.h \
 		$(SRC_DIR)/network/encoders.h \
+		$(SRC_DIR)/platform/signal-handler.h \
 		$(SRC_DIR)/json/json.h \
 		$(SRC_DIR)/registry/registry.h \
 		$(SRC_DIR)/resource/resource_id.h \
--- sources.mk
+++ sources.mk
@@ -1,4 +1,6 @@
-SRCS := $(SRC_DIR)/utils/string.c \
+SRCS := $(SRC_DIR)/main.c \
+		$(SRC_DIR)/logger.c \
+		$(SRC_DIR)/utils/string.c \
 		$(SRC_DIR)/utils/bitwise.c \
 		$(SRC_DIR)/memory/dyn_arena.c \
 		$(SRC_DIR)/memory/arena.c \
@@ -6,8 +8,7 @@ SRCS := $(SRC_DIR)/utils/string.c \
 		$(SRC_DIR)/containers/dict-op.c \
 		$(SRC_DIR)/containers/vector-op.c \
 		$(SRC_DIR)/containers/dict-init.c \
-		$(SRC_DIR)/main.c \
-		$(SRC_DIR)/logger.c \
+		$(SRC_DIR)/containers/ring_queue.c \
 		$(SRC_DIR)/network/handlers.c \
 		$(SRC_DIR)/network/network.c \
 		$(SRC_DIR)/network/decoders.c \
@@ -17,6 +18,7 @@ SRCS := $(SRC_DIR)/utils/string.c \
 		$(SRC_DIR)/network/encoders.c \
 		$(SRC_DIR)/network/bytebuffer.c \
 		$(SRC_DIR)/network/sender.c \
+		$(SRC_DIR)/platform/signal-handler.c \
 		$(SRC_DIR)/json/json.c \
 		$(SRC_DIR)/registry/registry.c \
 		$(SRC_DIR)/resource/resource_id.c \
