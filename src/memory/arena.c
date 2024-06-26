da5675dc9a39c5a1257727e5564bff60faef97e7
MrGhastien <bastounm@free.fr>
2024-06-27 01:17:25 +0200

    feat(arena): Add option to disable logging when creating an arena

--- src/memory/arena.c
+++ src/memory/arena.c
@@ -1,13 +1,19 @@
 #include "arena.h"
-#include "utils/bitwise.h"
 #include "logger.h"
+#include "utils/bitwise.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-Arena arena_create(size_t size) {
-    Arena res = {.block = malloc(size), .capacity = size, .length = 0, .saved_length = 0};
+Arena arena_create(u64 size) {
+    Arena res = {
+        .block = malloc(size),
+        .capacity = size,
+        .length = 0,
+        .saved_length = 0,
+        .logging = TRUE,
+    };
     if (!res.block)
         res.capacity = 0;
 
@@ -16,37 +22,67 @@ Arena arena_create(size_t size) {
     return res;
 }
 
+Arena arena_create_silent(u64 size) {
+    Arena res = {
+        .block = malloc(size),
+        .capacity = size,
+        .length = 0,
+        .saved_length = 0,
+        .logging = FALSE,
+    };
+    if (!res.block)
+        res.capacity = 0;
+
+    return res;
+}
+
 void arena_destroy(Arena* arena) {
     free(arena->block);
-    log_tracef("Destroyed arena %p (%zu / %zu).", arena->block, arena->length, arena->capacity);
+    if (arena->logging) {
+        log_tracef("Destroyed arena %p (%zu / %zu).", arena->block, arena->length, arena->capacity);
+    }
     arena->block = NULL;
 }
 
-void* arena_allocate(Arena* arena, size_t bytes) {
-    size_t remaining = arena->capacity - arena->length;
+void* arena_allocate(Arena* arena, u64 bytes) {
+    u64 remaining = arena->capacity - arena->length;
     if (bytes > remaining) {
-        log_errorf("Tried to allocate %zu bytes, but only %zu are available.", bytes,
-                arena->capacity - arena->length);
+        if (arena->logging)
+            log_errorf("Tried to allocate %zu bytes, but only %zu are available.",
+                       bytes,
+                       arena->capacity - arena->length);
         abort();
         return NULL;
     }
 
     void* ptr = offset(arena->block, arena->length);
     arena->length += bytes;
-    log_tracef("Allocated %zu bytes from %p (%zu/%zu).", bytes, arena->block, arena->length, arena->capacity);
+    if (arena->logging) {
+        log_tracef("Allocated %zu bytes from %p (%zu/%zu).",
+                   bytes,
+                   arena->block,
+                   arena->length,
+                   arena->capacity);
+    }
     return ptr;
 }
 
-void* arena_callocate(Arena* arena, size_t bytes) {
+void* arena_callocate(Arena* arena, u64 bytes) {
     void* ptr = arena_allocate(arena, bytes);
     return memset(ptr, 0, bytes);
 }
 
-void arena_free(Arena* arena, size_t bytes) {
+void arena_free(Arena* arena, u64 bytes) {
     if (bytes > arena->length)
         bytes = arena->length;
     arena->length -= bytes;
-    log_tracef("Free %zu bytes from %p (%zu/%zu).", bytes, arena->block, arena->length, arena->capacity);
+    if (arena->logging) {
+        log_tracef("Free %zu bytes from %p (%zu/%zu).",
+                   bytes,
+                   arena->block,
+                   arena->length,
+                   arena->capacity);
+    }
 }
 
 void arena_free_ptr(Arena* arena, void* ptr) {
@@ -70,6 +106,6 @@ void* arena_recent_pos(Arena* arena) {
     return offset(arena->block, arena->saved_length);
 }
 
-size_t arena_recent_length(Arena* arena) {
+u64 arena_recent_length(Arena* arena) {
     return arena->length - arena->saved_length;
 }
--- src/memory/arena.h
+++ src/memory/arena.h
@@ -1,6 +1,7 @@
 #ifndef ARENA_H
 #define ARENA_H
 
+#include "definitions.h"
 #include <stddef.h>
 
 /**
@@ -10,23 +11,25 @@
  */
 typedef struct arena {
     void* block;
-    size_t capacity;
-    size_t length;
-    size_t saved_length;
+    u64 capacity;
+    u64 length;
+    u64 saved_length;
+    bool logging;
 } Arena;
 
-Arena arena_create(size_t size);
+Arena arena_create(u64 size);
+Arena arena_create_silent(u64 size);
 void arena_destroy(Arena* arena);
 
-void* arena_allocate(Arena* arena, size_t bytes);
-void* arena_callocate(Arena* arena, size_t bytes);
-void arena_free(Arena* arena, size_t bytes);
+void* arena_allocate(Arena* arena, u64 bytes);
+void* arena_callocate(Arena* arena, u64 bytes);
+void arena_free(Arena* arena, u64 bytes);
 void arena_free_ptr(Arena* arena, void* ptr);
 
 void arena_save(Arena* arena);
 void arena_restore(Arena* arena);
 
-size_t arena_recent_length(Arena* arena);
+u64 arena_recent_length(Arena* arena);
 void* arena_recent_pos(Arena* arena);
 
 #endif /* ! ARENA_H */
