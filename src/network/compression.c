8bf48dbf18ca387fabc31da3e03496ae275208b0
Bastien Morino <bastien.morino@free.fr>
2024-09-20 00:03:13 +0200

    refactor(network/compression): Simplify the compression code
    Make use of contiguous regions in bytebuffers through previously added functions

--- src/network/compression.c
+++ src/network/compression.c
@@ -2,11 +2,11 @@
 #include "containers/bytebuffer.h"
 #include "logger.h"
 #include "memory/arena.h"
-#include "utils/bitwise.h"
-#include "utils/math.h"
 #include <zlib.h>
 
-typedef i32 (*zlib_action)(z_streamp stream);
+#define CHUNK 16384
+
+typedef i32 (*zlib_action)(z_streamp stream, int flush);
 typedef i32 (*zlib_resetter)(z_streamp stream);
 
 static void* zlib_alloc(void* arena, u32 item_count, u32 size) {
@@ -29,13 +29,13 @@ bool compression_init(CompressionContext* ctx, Arena* arena) {
     ctx->inflate_stream.opaque = arena;
 
     i32 code = deflateInit(&ctx->deflate_stream, Z_DEFAULT_COMPRESSION);
-    if(code != Z_OK) {
+    if (code != Z_OK) {
         log_errorf("ZLib: %s.", ctx->deflate_stream.msg);
         log_error("Failed to initialize the compression context.");
         return FALSE;
     }
     code = inflateInit(&ctx->inflate_stream);
-    if(code != Z_OK) {
+    if (code != Z_OK) {
         log_errorf("ZLib: %s.", ctx->inflate_stream.msg);
         log_error("Failed to initialize the compression context.");
         return FALSE;
@@ -48,12 +48,12 @@ void compression_cleanup(CompressionContext* ctx) {
     inflateEnd(&ctx->inflate_stream);
 }
 
-static i32 zlib_deflate(z_streamp stream) {
-    return deflate(stream, Z_NO_FLUSH);
+static i32 zlib_deflate(z_streamp stream, int flush) {
+    return deflate(stream, flush);
 }
 
-static i32 zlib_inflate(z_streamp stream) {
-    return inflate(stream, Z_NO_FLUSH);
+static i32 zlib_inflate(z_streamp stream, int flush) {
+    return inflate(stream, flush);
 }
 
 static i32 zlib_reset_deflate(z_streamp stream) {
@@ -64,66 +64,63 @@ static i32 zlib_reset_inflate(z_streamp stream) {
     return inflateReset(stream);
 }
 
-static i64 zlib_execute(CompressionContext* ctx,
+static i64 zlib_execute(z_streamp stream,
                         ByteBuffer* out_buffer,
                         ByteBuffer* in_buffer,
                         zlib_action action,
                         zlib_resetter resetter) {
-    u64 to_compress = min_u64(in_buffer->capacity - in_buffer->read_head, in_buffer->size);
-    u64 to_write = min_u64(out_buffer->capacity - out_buffer->write_head,
-                           out_buffer->capacity - out_buffer->size);
-    bool in_split = to_compress < in_buffer->size;
 
-    ctx->deflate_stream.next_in = offset(in_buffer->buf, in_buffer->read_head);
-    ctx->deflate_stream.avail_in = to_compress;
-    ctx->deflate_stream.next_out = offset(out_buffer->buf, out_buffer->write_head);
-    ctx->deflate_stream.avail_out = to_write;
+    void* tmp;
+    int flush;
+
+    u8 outBuf[CHUNK];
+
+    /*
+      Here we don't copy any data from the input buffer.
+      Pointers to areas inside the buffers are given to ZLib to read.
+     */
+    stream->avail_out = CHUNK;
+    stream->next_out = outBuf;
 
     i32 res = Z_OK;
-    bool can_compress = TRUE;
-    while (can_compress) {
+    do {
+        stream->avail_in = bytebuf_contiguous_read(in_buffer, &tmp);
+        stream->next_in = tmp;
+        /*
+          We have to tell ZLib that it reached the end of the input data !
+          */
+        flush = in_buffer->size == 0 ? Z_FINISH : Z_NO_FLUSH;
 
         while (res == Z_OK) {
-            res = action(&ctx->deflate_stream);
+            res = action(stream, flush);
         }
         switch (res) {
-        case Z_STREAM_END:
-            if (in_split) {
-                ctx->deflate_stream.avail_in = in_buffer->size - to_compress;
-                ctx->deflate_stream.next_in = in_buffer->buf;
-                in_split = FALSE;
-            } else
-                can_compress = FALSE;
-            break;
-
         case Z_BUF_ERROR:
-            if (ctx->deflate_stream.total_out >= out_buffer->capacity - out_buffer->size) {
-                log_error("Compression output buffer is too small.");
-                return -1;
-            }
-            ctx->deflate_stream.avail_out = out_buffer->size - to_write;
-            ctx->deflate_stream.next_out = out_buffer->buf;
+            bytebuf_write(out_buffer, outBuf, CHUNK - stream->avail_out);
+            stream->avail_out = CHUNK;
+            stream->next_out = outBuf;
             break;
 
-        default:
-            log_error("Error when compressing packet.");
+        case Z_STREAM_ERROR:
+            log_error("Error when (de)compressing packet.");
             return -1;
         }
-    }
-    u64 written = ctx->deflate_stream.total_out;
-    out_buffer->size += written;
-    out_buffer->write_head += written;
-    if (resetter(&ctx->deflate_stream) != Z_OK) {
+    } while (flush != Z_FINISH);
+
+    bytebuf_write(out_buffer, outBuf, CHUNK - stream->avail_out);
+    if (resetter(stream) != Z_OK) {
         log_error("Could not end the compression stream.");
         return -1;
     }
-    return written;
+    return stream->total_out;
 }
 
 i64 compression_compress(CompressionContext* ctx, ByteBuffer* out_buffer, ByteBuffer* in_buffer) {
-    return zlib_execute(ctx, out_buffer, in_buffer, &zlib_deflate, &zlib_reset_deflate);
+    return zlib_execute(
+        &ctx->deflate_stream, out_buffer, in_buffer, &zlib_deflate, &zlib_reset_deflate);
 }
 
 i64 compression_decompress(CompressionContext* ctx, ByteBuffer* out_buffer, ByteBuffer* in_buffer) {
-    return zlib_execute(ctx, out_buffer, in_buffer, &zlib_inflate, &zlib_reset_inflate);
+    return zlib_execute(
+        &ctx->inflate_stream, out_buffer, in_buffer, &zlib_inflate, &zlib_reset_inflate);
 }
