60143a3acfa826b497c19868ebbb35b91e6ea8da
Bastien Morino <bastien.morino@free.fr>
2025-02-17 02:47:41 +0100

    fix(platform): Include missing header needd for linux time functions

--- src/platform/linux/time_linux.c
+++ src/platform/linux/time_linux.c
@@ -5,6 +5,7 @@
 #ifdef MC_PLATFORM_LINUX
 
 #include "platform/time.h"
+#include <unistd.h>
 
 bool timestamp(struct timespec *out_timestamp) {
     return clock_gettime(CLOCK_MONOTONIC, out_timestamp) == 0;
