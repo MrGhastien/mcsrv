a15af68d1cd16760ae9978a414a9e7700b969523
Bastien Morino <bastien.morino@free.fr>
2025-06-12 15:11:08 +0200

    feat(memory): Implement a buddy allocator.

---
+++ src/memory/allocators/buddy.c
@@ -0,0 +1,86 @@
+#include "buddy.h"
+#include "memory/_memory_internal.h"
+#include "utils/bitwise.h"
+
+#define MIN_BLOCK_SIZE 5
+#define MAX_BLOCK_SIZE 12
+
+typedef struct BuddyBlock {
+    u32 power_size;
+    bool free;
+    //memory_block parent_slab;
+} BuddyBlock;
+
+static BuddyBlock* buddy_block_next(BuddyBlock* block) {
+    return offset(block, 1ULL << (block->power_size + MIN_BLOCK_SIZE));
+}
+
+void buddy_init(BuddyAllocator* alloc, u64 size, enum MemoryChainTag tag) {
+
+    size = ceil_two_pow(size + sizeof(BuddyBlock));
+
+    alloc->chain = create_chain(tag, INVALID_CHAIN);
+    memory_block blk = alloc_block(size, alloc->chain);
+    alloc->head = block_memory(blk);
+    alloc->tail = buddy_block_next(alloc->head);
+
+    alloc->head->power_size = u64_log2(size) - MIN_BLOCK_SIZE;
+    alloc->head->free = TRUE;
+}
+void buddy_destroy(BuddyAllocator* alloc) {
+    destroy_chain(alloc->chain);
+}
+
+static BuddyBlock* block_split(BuddyBlock* block, u64 requested_size) {
+    if (!block || requested_size == 0 || !block->free)
+        return NULL;
+
+    while ((1ULL << (block->power_size + MIN_BLOCK_SIZE)) > requested_size) {
+        u64 split_size = block->power_size - 1;
+        block->power_size = split_size;
+        BuddyBlock* next = buddy_block_next(block);
+        next->power_size = split_size;
+        next->free = TRUE;
+    }
+
+    return block;
+}
+
+static void block_merge(BuddyAllocator* alloc, BuddyBlock* block) {
+    if (!block->free)
+        return;
+
+    while (block != alloc->tail) {
+        BuddyBlock* next = buddy_block_next(block);
+        if (next->power_size != block->power_size || !next->free)
+            return;
+        /*
+        if (next->parent_slab != block->parent_slab)
+            return;
+        */
+
+        block->power_size++;
+    }
+}
+
+void* buddy_alloc(BuddyAllocator* alloc, u64 size) {
+    u64 actual_size = size + sizeof(BuddyBlock);
+
+    // Search for available block
+    BuddyBlock* blk = alloc->head;
+    while (blk != alloc->tail &&
+           ((1ULL << (blk->power_size + MIN_BLOCK_SIZE)) < actual_size || !blk->free)) {
+        blk = buddy_block_next(blk);
+    }
+
+    blk = block_split(blk, actual_size);
+
+    return offset(blk, sizeof(BuddyBlock));
+}
+
+void buddy_free(BuddyAllocator* alloc, void* ptr) {
+    BuddyBlock* blk = offset(ptr, -sizeof(BuddyBlock));
+    blk->free = TRUE;
+
+    block_merge(alloc, blk);
+}
---
+++ src/memory/allocators/buddy.h
@@ -0,0 +1,56 @@
+/**
+ * @file buddy.h
+ * @author Bastien Morino
+ * @brief Buddy allocator.
+ *
+ * @see https://wikipedia.org/wiki/Buddy_memory_allocation
+ */
+
+#ifndef BUDDY_H
+#define BUDDY_H
+
+#include "memory/_memory_internal.h"
+
+struct BuddyBlock;
+
+typedef struct BuddyAllocator {
+    memory_chain chain;
+    struct BuddyBlock* head;
+    struct BuddyBlock* tail;
+} BuddyAllocator;
+
+/**
+ * Initializes a buddy allocator.
+ *
+ * The @p size parameter is rounder up to the nearest power of two, starting at 2⁵ (32).
+ *
+ * @param[out] alloc A pointer to the allocator object to initialize.
+ * @param[in] size The total size of the allocator, rounded up to the nearest power of two.
+ * @param[in] tag A tag to apply to the underlying memory allocation(s).
+ */
+void buddy_init(BuddyAllocator* alloc, u64 size, enum MemoryChainTag tag);
+
+/**
+ * Tears down a buddy allocator.
+ *
+ * @param[inout] alloc A pointer to the allocator object to tear down.
+ */
+void buddy_destroy(BuddyAllocator* alloc);
+
+/**
+ * Allocates memory from a buddy allocator.
+ *
+ * @param[inout] alloc The allocator to use.
+ * @param[in] size The size of the allocation to make.
+ * @returns A pointer to the allocated memory, or `NULL` if a problem occurred.
+ */
+void* buddy_alloc(BuddyAllocator* alloc, u64 size);
+/**
+ * Frees memory from a buddy allocator.
+ *
+ * @param[inout] alloc The allocator to use.
+ * @param[in] ptr A pointer to the memory to free. Must not be @p NULL.
+ */
+void buddy_free(BuddyAllocator* alloc, void* ptr);
+
+#endif /* ! BUDDY_H */
