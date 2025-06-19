d600f8c2bd98d3ee5b3eee112c913773e6ced21f
Bastien Morino <bastien.morino@free.fr>
2025-06-20 00:01:14 +0200

    fix(containers): Check if dict capacity is a power of two when init
    The capacity must be a power of two because of the formula used to
    simulate random traversal of the entire element array. It only goes
    through all indices once when the capacity is a power of two.

--- src/containers/dict-init.c
+++ src/containers/dict-init.c
@@ -1,4 +1,6 @@
 #include "dict.h"
+#include "logger.h"
+#include "platform/platform.h"
 #include "utils/hash.h"
 #include "memory/mem_tags.h"
 
@@ -7,6 +9,13 @@
 #define DEFAULT_CAP 4
 
 void dict_init_fixed(Dict* map, const Comparator* cmp, Arena* arena, u64 capacity, u64 key_stride, u64 value_stride) {
+
+    // Check if capacity is null or not a power of two
+    if (capacity == 0 || (capacity & (capacity - 1)) != 0) {
+        log_fatalf("Dictionnary capacity is not a power of two (%lu)", capacity);
+        platform_abort();
+    }
+
     map->base = arena_callocate(
         arena, capacity * (sizeof(u64) + key_stride + value_stride)/* , ALLOC_TAG_DICT */);
     map->comparator = cmp;
