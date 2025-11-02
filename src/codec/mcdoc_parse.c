a000c67c341931066904e85a47df024e5480a75f
Bastien Morino <bastien.morino@free.fr>
2025-11-03 00:16:08 +0100

    feat: Start implementing a parser for mcdoc

---
+++ src/codec/mcdoc_parse.c
@@ -0,0 +1,19 @@
+#include "containers/vector.h"
+
+enum McdocTokenType {
+    MCDOC_INTEGER,
+    MCDOC_FLOAT,
+    MCDOC_RANGE,
+    MCDOC_STRING,
+    MCDOC_RESID,
+    MCDOC_IDENTIFIER,
+    MCDOC_PATH,
+};
+
+static void mcdoc_tokenize(Vector* token_list) {
+
+}
+
+void mcdoc_parse(const char* filename) {
+    
+}
